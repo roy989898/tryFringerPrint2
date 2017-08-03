276396849d0e497d0db64f36c380a60e7c36d0d5
roy989898 <roy989898@gmail.com>
2017-08-03 15:07:55 +0800

    add a button

--- app/src/main/java/com/roy/tryfringerprint/MainActivity.java
+++ app/src/main/java/com/roy/tryfringerprint/MainActivity.java
@@ -1,13 +1,34 @@
 package com.roy.tryfringerprint;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+
+    private Button btFingerPrint;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        btFingerPrint = (Button) findViewById(R.id.bt_finger_print);
+        btFingerPrint.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.bt_finger_print:
+                break;
+        }
+
     }
 }
