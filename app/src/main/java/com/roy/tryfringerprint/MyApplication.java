4eef17dbcb329cbf4b9413f187088b18d9bd3b1e
roy989898 <roy989898@gmail.com>
2017-08-03 15:20:19 +0800

    add MyApplication

---
+++ app/src/main/java/com/roy/tryfringerprint/MyApplication.java
@@ -0,0 +1,15 @@
+package com.roy.tryfringerprint;
+
+import android.app.Application;
+
+/**
+ * Created by roy.leung on 3/8/2017.
+ */
+
+public class MyApplication extends Application{
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
