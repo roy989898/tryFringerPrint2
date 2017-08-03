d1382011ea2aec587455d655459d0d87e54c929e
roy989898 <roy989898@gmail.com>
2017-08-03 15:14:47 +0800

    im[lemengt the fringer print

--- app/src/main/java/com/roy/tryfringerprint/MainActivity.java
+++ app/src/main/java/com/roy/tryfringerprint/MainActivity.java
@@ -1,19 +1,26 @@
 package com.roy.tryfringerprint;
 
+import android.hardware.fingerprint.FingerprintManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+import com.multidots.fingerprintauth.FingerPrintAuthCallback;
+import com.multidots.fingerprintauth.FingerPrintAuthHelper;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, FingerPrintAuthCallback {
 
 
     private Button btFingerPrint;
+    private FingerPrintAuthHelper mFingerPrintAuthHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mFingerPrintAuthHelper = FingerPrintAuthHelper.getHelper(this, this);
         initView();
     }
 
@@ -27,8 +34,54 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.bt_finger_print:
+                mFingerPrintAuthHelper.startAuth();
                 break;
         }
+
+    }
+
+    @Override
+    protected void onPause() {
+        if (mFingerPrintAuthHelper != null)
+            stopTheAuth(mFingerPrintAuthHelper);
+
+        super.onPause();
+    }
+
+    @Override
+    public void onNoFingerPrintHardwareFound() {
+        Toast.makeText(this, "No Hardware", Toast.LENGTH_LONG).show();
+        stopTheAuth(mFingerPrintAuthHelper);
+
+    }
+
+    @Override
+    public void onNoFingerPrintRegistered() {
+        Toast.makeText(this, "onNoFingerPrintRegistered", Toast.LENGTH_LONG).show();
+        stopTheAuth(mFingerPrintAuthHelper);
+
+    }
+
+    @Override
+    public void onBelowMarshmallow() {
+        Toast.makeText(this, "onBelowMarshmallow", Toast.LENGTH_LONG).show();
+        stopTheAuth(mFingerPrintAuthHelper);
+    }
+
+    @Override
+    public void onAuthSuccess(FingerprintManager.CryptoObject cryptoObject) {
+        Toast.makeText(this, "onAuthSuccess", Toast.LENGTH_LONG).show();
+        stopTheAuth(mFingerPrintAuthHelper);
+    }
+
+    @Override
+    public void onAuthFailed(int errorCode, String errorMessage) {
+        Toast.makeText(this, "onAuthFailed" + " " + errorCode + " " + errorMessage, Toast.LENGTH_LONG).show();
+        stopTheAuth(mFingerPrintAuthHelper);
+    }
+
+    private void stopTheAuth(FingerPrintAuthHelper fingerPrintAuthHelper) {
+        fingerPrintAuthHelper.stopAuth();
 
     }
 }
